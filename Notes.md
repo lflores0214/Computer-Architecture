307e529a3b422db0f617c22a2ea7fbf464b1d49a
Luis Floresgit config --global user.name Luis <Lflores02142@gmail.com>
2020-04-20 14:12:01 -0700

    initial commit

---
+++ Notes.md
@@ -0,0 +1,81 @@
+
+
+Number Bases
+-------------
+
+12 apples
+0xC apples
+0b1100 apples
+
+Binary    Base 2  0-1
+Octal     Base 8  0-7
+Decimal   Base 10 0-9
+Hex       Base 16 0-9 a-f
+          Base 64 0-9 a-f A-F +/
+
+assumptions are base 10
+a = 123 base 10
+0b indicates binary
+0x indicates hexidecimal
+
+# Base 10
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+
+1234
+
+one 1000
+two 100
+three 10
+1 4
+
+1 * 1000 +
+2 * 100 +
+3 * 10 +
+4 * 1
+
+# Binary (base 2)
+
++-----8's place (0b1000's place)
+|+----4's place (0b100's place)
+||+---2's place (0b10's place)
+|||+--1's place (0b1's place)
+||||
+   0
+   1
+  10
+  11
+ 100
+ 101
+ 110
+ 111
+1000
+
+Binary to Hex
+--------------
+
+4 binary digits == 1 hex digit
+  ^^         ^
+Byte is 8 bits
+
+11010011
+
+1101 0011
+  d    3
+
+0b 11010011 == 0xd3
+
+0xff
+
+ f      f
+1111   1111
+
+0b11111111 == 255 == 0xff
---
+++ comp.py
@@ -0,0 +1,41 @@
+# write a python program that runs programs
+
+PRINT_LUIS = 1
+HALT = 2
+SAVE_REG = 3 # Store a value in a register ( in the LS8 called LDI)
+PRINT_REG  = 4 # corresponds to PRN in the LS8
+memory = [PRINT_LUIS,
+          SAVE_REG, # save R0,37 store 37 in R0 the opcode
+          0, #R0 operand ("argument")
+          37, #37 operand
+          PRINT_LUIS,
+          PRINT_REG, # PRINT_REG R0
+          0, # R0
+          HALT
+          ]
+register = [0] * 8 # like variables R0-R7
+
+pc = 0  # Program Counter, the address of the current instruction
+running = True
+
+while running:
+    inst = memory[pc]
+
+    if inst == PRINT_LUIS:
+        print("Luis")
+        pc += 1
+    elif inst == SAVE_REG:
+        reg_num = memory[pc+1]
+        value = memory[pc+2]
+        register[reg_num] = value
+        pc += 3
+    elif inst == PRINT_REG:
+        reg_num = memory[pc + 1]
+        value = register[reg_num]
+        print(value)
+        pc += 2 
+    elif inst == HALT:
+        running = False
+    else: 
+        print("unknown instruction")
+        running = False
